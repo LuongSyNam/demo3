042325d6af03cbf5f1f6017232648e06a814c6db
PC <PC@DESKTOP-1MKJ06O>
2024-11-20 16:00:55 +0700

    Third Commit

--- demo3/src/main/java/vn/iotstar/entity/Users.java
+++ demo3/src/main/java/vn/iotstar/entity/Users.java
@@ -3,16 +3,7 @@ package vn.iotstar.entity;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
